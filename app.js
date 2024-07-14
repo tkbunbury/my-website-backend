b11993f7ee9e9c1aa5d4064a82a6b0375145d6f1
Takai <tkbunbury@gmail.com>
2024-07-14 19:00:49 +0100

    updated vercel file

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ const {
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// app.use(cors())
+app.use(cors())
 
-app.use(cors({
-    origin: 'http://127.0.0.1:3000', // Update this for your production URL later
-}));
+// app.use(cors({
+//     origin: 'http://127.0.0.1:3000', // Update this for your production URL later
+// }));
 
 app.post('/api/send-message', sendMessage);
 
